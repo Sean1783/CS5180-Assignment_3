6a5d4f9b9c050471d05171c231575a170e35e0a0
Sean1783 <86225444+Sean1783@users.noreply.github.com>
2024-11-17 11:43:04 -0800

    Final version for questions 4, 5. Needs cleanup.

--- BeautifulSoup/crawler.py
+++ BeautifulSoup/crawler.py
@@ -1,5 +1,6 @@
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
 
@@ -16,11 +17,11 @@ class Crawler:
                 bs = BeautifulSoup(html.read(), 'html.parser')
                 return bs.find_all('a')
         except HTTPError as e:
-            print(f"HTTP error: {e}" + link_to_visit)
+            print(f"HTTP error: {e} " + link_to_visit)
         except URLError as e:
-            print(f"URL error: {e}" + link_to_visit)
+            print(f"URL error: {e} " + link_to_visit)
         except Exception as e:
-            print(f"An unexpected error occurred: {e}" + link_to_visit)
+            print(f"An unexpected error occurred: {e} " + link_to_visit)
         return []
 
 
@@ -30,40 +31,23 @@ class Crawler:
             url_string = individual_url.get('href')
             if url_string and url_string.endswith(('.html', '.shtml')):
                 filtered_url_list.append(url_string)
-                # print(url_string)
         return filtered_url_list
 
 
-    def construct_full_links(self, filtered_url_list):
-        frontier_urls = set()
-        for link in filtered_url_list:
-            if self.is_correct_domain(link):
-                if link.startswith('/'):
-                    full_link = self.base_url + link
-                elif link.startswith(self.base_url):
-                    full_link = link
-                else:
-                    full_link = self.base_url + '/' + link
-                # print(full_link)
-                frontier_urls.add(full_link)
-        return list(frontier_urls)
-
-
-    def is_correct_domain(self, link):
-        scheme = 'http://'
-        scheme_secure = 'https://'
-        if link.startswith(self.base_url):
-            return True
-        elif link.startswith(scheme) or link.startswith(scheme_secure):
-            return False
-        else:
-            return True
+    def construct_correct_urls(self, current_page_url, filtered_url_list):
+        absolute_urls = []
+        for href in filtered_url_list:
+            if not href.startswith('http'):
+                absolute_urls.append(urljoin(current_page_url, href))
+            else:
+                absolute_urls.append(href)
+        return absolute_urls
 
 
     def generate_new_frontier_urls(self, current_link):
         anchor_tags = self.visit_link_and_gather_anchor_tags(current_link)
         raw_list = self.create_list_of_raw_links(anchor_tags)
-        frontier = self.construct_full_links(raw_list)
+        frontier = self.construct_correct_urls(current_link, raw_list)
         return frontier
 
 
@@ -105,3 +89,46 @@ class Crawler:
             for next_link in additional_frontier:
                 if next_link not in base_frontier:
                     base_frontier.append(next_link)
+
+
+    # Deprecated.
+    def construct_full_links(self, filtered_url_list):
+        frontier_urls = set()
+        for link in filtered_url_list:
+            # print(link)
+            full_link = ""
+            if link.startswith('/'):
+                full_link = self.base_url + link
+            elif link.startswith(self.base_url):
+                full_link = link
+            elif link.startswith('..'):
+                link = link.replace('..', '/sci/computer-science')
+                full_link = self.base_url + link
+            else:
+                print(link)
+                full_link = self.base_url + '/' + link
+                # print(full_link)
+            if self.is_correct_domain(full_link):
+                frontier_urls.add(full_link)
+        return list(frontier_urls)
+
+
+    # Deprecated.
+    # Returns false for outbound links. Prevents outbound URLs from being added to the frontier.
+    def is_correct_domain(self, link):
+        scheme = 'http://'
+        scheme_secure = 'https://'
+        if link.startswith(self.base_url):
+            return True
+        elif link.startswith(scheme) or link.startswith(scheme_secure):
+            return False
+        else:
+            return True
+
+
+    # Deprecated.
+    def generate_frontier(self, current_link):
+        anchor_tags = self.visit_link_and_gather_anchor_tags(current_link)
+        raw_list = self.create_list_of_raw_links(anchor_tags)
+        frontier = self.construct_full_links(raw_list)
+        return frontier
